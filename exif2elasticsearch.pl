3b4a3037ad13c5b37040b4948c1ffc7483fa1578
Leon Brocard <acme@astray.com>
2013-01-29 14:40:50 +0000

    Sequence number

--- exif2elasticsearch.pl
+++ exif2elasticsearch.pl
@@ -27,7 +27,7 @@ foreach my $filename (keys %$data) {
       if ($exif->{Canon_CameraTemperature} <= 24) {
         $temperature = '19to24';
       } elsif ($exif->{Canon_CameraTemperature} <= 28) {
-        $temperature = '25to28',
+        $temperature = '25to28';
       } else {
         $temperature = '29to34';
       }
@@ -39,9 +39,9 @@ foreach my $filename (keys %$data) {
       if ($exif->{Canon_FocusDistanceUpper} <= 0.3) {
         $distance = '0to0d3';
       } elsif ($exif->{Canon_FocusDistanceUpper} <= 9) {
-        $distance = '0d3to9',
+        $distance = '0d3to9';
       } else {
-        $distance = '10to100',
+        $distance = '10to100';
       }
       $exif->{distance} = $distance;
     }
@@ -50,9 +50,9 @@ foreach my $filename (keys %$data) {
       if ($exif->{Canon_ExposureTime} <= 0.01) {
         $exposuretime = 'low';
       } elsif ($exif->{Canon_ExposureTime} <= 0.1) {
-        $exposuretime = 'medium',
+        $exposuretime = 'medium';
       } else {
-        $exposuretime = 'high',
+        $exposuretime = 'high';
       }
       $exif->{exposuretime} = $exposuretime;
     }
@@ -62,11 +62,10 @@ foreach my $filename (keys %$data) {
       if ($exif->{GPS_GPSAltitude} <= 10) {
         $altitude = 'low';
       } elsif ($exif->{GPS_GPSAltitude} <= 100) {
-        $altitude = 'medium',
+        $altitude = 'medium';
       } else {
-        $altitude = 'high',
+        $altitude = 'high';
       }
-      say $altitude;
       $exif->{altitude} = $altitude;
     }
     if (defined $exif->{Canon_OpticalZoomCode}) {
@@ -74,11 +73,22 @@ foreach my $filename (keys %$data) {
       if ($exif->{Canon_OpticalZoomCode} <= 0) {
         $zoom = 'low';
       } elsif ($exif->{Canon_OpticalZoomCode} < 120) {
-        $zoom = 'medium',
+        $zoom = 'medium';
       } else {
-        $zoom = 'high',
+        $zoom = 'high';
       }
       $exif->{zoom} = $zoom;
+    }
+    if (defined $exif->{Canon_SequenceNumber}) {
+      my $sequence;
+      if ($exif->{Canon_SequenceNumber} <= 1) {
+        $sequence = 'low';
+      } elsif ($exif->{Canon_SequenceNumber} <= 5) {
+        $sequence = 'medium',
+      } else {
+        $sequence = 'high',
+      }
+      $exif->{sequence} = $sequence;
     }
     foreach my $field ('Canon_AFPointsInFocus') {
         delete $exif->{$field};
